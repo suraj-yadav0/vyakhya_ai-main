770f8a719b36206542e61b670b4c206fc15689b8
Suraj Yadav <surajyadav200701@gmail.com>
2024-03-30 13:55:16 +0530

    added the material theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home: const SplashScreen(),
     );
