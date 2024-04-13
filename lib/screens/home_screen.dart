f4a4084923092c576ab2076912802dabf31e50ea
Suraj Yadav <surajyadav200701@gmail.com>
2024-04-13 20:05:51 +0530

    Added the drawer , might add the functionalties later

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -61,6 +61,32 @@ class _HomeScreenState extends State<HomeScreen> {
                 .toList(),
           ).animate().fade(duration: 1.seconds),
         ),
+        drawer: const Drawer(
+        backgroundColor: Colors.teal,
+        child: Column(
+          children: [
+             DrawerHeader(
+        child: Icon(Icons.favorite),
+      ),
+       ListTile(
+        leading: Icon(Icons.home),
+        title: Text('D A S H B O A R D'),
+      ),
+      ListTile(
+        leading: Icon(Icons.chat),
+        title: Text('C H A T'),
+      ),
+      ListTile(
+        leading: Icon(Icons.settings),
+        title: Text('S E T T I N G S'),
+      ),
+      ListTile(
+        leading: Icon(Icons.logout_outlined),
+        title: Text('L O G O U T'),
+      )
+          ],
+        ),
+      ),
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -102,6 +102,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           ],
         ),
       ),
+      
     );
   }
 }
