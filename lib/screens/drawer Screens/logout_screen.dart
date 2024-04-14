8aa01e804db964b95437f69b54b050df256695b9
Suraj Yadav <surajyadav200701@gmail.com>
2024-04-14 10:44:54 +0530

    logout Screen Added.

---
+++ lib/screens/drawer Screens/logout_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:vyakhya_ai/widgets/Custom%20Scaffolds/custom_drawer_screen.dart';
+
+class LogoutScreen extends StatelessWidget {
+  const LogoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomDrawerScreen(title: "L o g O u t", body: Center(
+        child: Text(
+          "Login Again Will be Displayed Here.",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),);
+  }
+}
