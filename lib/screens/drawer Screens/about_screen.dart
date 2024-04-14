07ab4492ac3d9ac033fa68846afd34e28af3d0ea
Suraj Yadav <surajyadav200701@gmail.com>
2024-04-14 18:10:11 +0530

    almost completed the UI of About Section

--- lib/screens/drawer Screens/about_screen.dart
+++ lib/screens/drawer Screens/about_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vyakhya_ai/helper/global.dart';
+
 import 'package:vyakhya_ai/widgets/Custom%20Scaffolds/custom_drawer_screen.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -9,11 +9,67 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const CustomDrawerScreen(
       title: 'A b o u t',
-      body: Center(
-        child: Text(
-          "THE ABOUT SECTION",
-          style: TextStyle(color: Colors.white),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "Vyakya AI v.0.0",
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  wordSpacing: 4.5),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "Vyakhya AI stands as a cutting-edge solution, revolutionizing language translation with its robust architecture and user-friendly interface. As we anticipate Vyakhya AI 2.0 and beyond, the integration of quantum computing, expanded language pairs, and advancements in emotion recognition promises a future where linguistic barriers dissolve. Vyakhya AI's commitment to innovation, inclusivity, and security positions it as a pivotal force in global connectivity.\n",
+              style:
+                  TextStyle(color: Colors.white, fontSize: 16, wordSpacing: 2),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Developed By :",
+            style: TextStyle(color: Colors.white, fontSize: 20, wordSpacing: 2),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+           Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "S U R A J Y A D A V",
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  wordSpacing: 4.5),
+            ),
+          ),
+           SizedBox(
+            height: 10,
+          ),
+           Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "(2129753)",
+              style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  wordSpacing: 4.5),
+            ),
+          ),
+        ],
       ),
     );
   }
