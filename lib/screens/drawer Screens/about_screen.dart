d05002d3bd23806f901b82ffd1ec859f3b94dc5a
Suraj Yadav <surajyadav200701@gmail.com>
2024-04-15 12:11:33 +0530

    mad echanges in the about section

--- lib/screens/drawer Screens/about_screen.dart
+++ lib/screens/drawer Screens/about_screen.dart
@@ -9,67 +9,114 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const CustomDrawerScreen(
       title: 'A b o u t',
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              "Vyakya AI",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.w700,
-                  wordSpacing: 4.5),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "Vyakya AI",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                    wordSpacing: 4.5),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              "Vyakhya AI stands as a cutting-edge solution, revolutionizing language translation with its robust architecture and user-friendly interface. As we anticipate Vyakhya AI 2.0 and beyond, the integration of quantum computing, expanded language pairs, and advancements in emotion recognition promises a future where linguistic barriers dissolve. Vyakhya AI's commitment to innovation, inclusivity, and security positions it as a pivotal force in global connectivity.\n",
-              style:
-                  TextStyle(color: Colors.white, fontSize: 16, wordSpacing: 2),
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Developed By :",
-            style: TextStyle(color: Colors.teal, fontSize: 20, wordSpacing: 2),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-           Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Text(
-              "S U R A J Y A D A V",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  wordSpacing: 4.5),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "Vyakhya AI stands as a cutting-edge solution, revolutionizing language translation with its robust architecture and user-friendly interface. As we anticipate Vyakhya AI 2.0 and beyond, the integration of quantum computing, expanded language pairs, and advancements in emotion recognition promises a future where linguistic barriers dissolve. Vyakhya AI's commitment to innovation, inclusivity, and security positions it as a pivotal force in global connectivity.\n",
+                style:
+                    TextStyle(color: Colors.white, fontSize: 16, wordSpacing: 2),
+              ),
             ),
-          ),
-           SizedBox(
-            height: 5,
-          ),
-           Padding(
-            padding: EdgeInsets.all(4.0),
-            child: Text(
-              "(2129753)",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  wordSpacing: 4.5),
+            SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Text(
+              "Developed By :",
+              style: TextStyle(color: Colors.teal, fontSize: 20, wordSpacing: 2),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+             Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "S U R A J Y A D A V",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 4.5),
+              ),
+            ),
+             SizedBox(
+              height: 5,
+            ),
+             Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "(2129753)",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 4.5),
+              ),
+            ),
+              SizedBox(
+              height: 5,
+            ),
+             Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "&",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 4.5),
+              ),
+            ),
+                         SizedBox(
+              height: 5,
+            ),
+             Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "Sneha Satapathy",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 4.5),
+              ),
+            ),
+                         SizedBox(
+              height: 5,
+            ),
+             Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
+                "(2129747)",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    wordSpacing: 4.5),
+              ),
+            ),
+              SizedBox(
+              height: 5,
+            ),
+          ],
+        ),
       ),
     );
   }
