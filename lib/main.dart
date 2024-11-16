a2751f25f714bdab772d6c3ffa6090af7b20f3a4
Suraj Yadav <surajyadav200701@gmail.com>
2024-11-16 23:32:47 +0530

    Starting with Different Devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:vyakhya_ai/screens/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+
+    backgroundColor:  Colors.white,
+     // Enable preview by default for web demo
+      enabled: true,
+  // Start with Galaxy A50 as it's a common Android device
+      defaultDevice: Devices.ios.iPhone13ProMax,
+
+      //show toolbar to let users test different devices
+
+
+
+    
+
+
+    builder: (context) => MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
